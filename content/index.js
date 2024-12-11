eaffdefb13a0cf13ebbc14cbdf68e62e11aaace8
Neeraj Fulpatiya <neerajfulpatiya555@gmail.com>
2024-12-11 21:25:44 +0530

    js for explore more icon

--- content/index.js
+++ content/index.js
@@ -107,3 +107,23 @@ function cross(){
   navlink.classList.remove('show');
 }
 /*--js for cross-icon ends--*/
+
+/*--js for show more projects starts--*/
+function project() {
+  let seeMoreBtn = document.querySelector("#see-more-btn");
+  let projectSection = document.querySelector("#project-section");
+  let projectContainer2 = document.querySelector("#project-container2");
+
+  if (projectContainer2.classList.contains('showmore')) {
+    // Hide the extra content and change button back
+    projectContainer2.classList.remove('showmore');
+    projectSection.classList.remove('height');
+    seeMoreBtn.innerHTML = 'See More <i class="fa-solid fa-angles-down" id="see-more-icon"></i>';
+  } else {
+    // Show the extra content and update the button text
+    projectContainer2.classList.add('showmore');
+    projectSection.classList.add('height');
+    seeMoreBtn.innerHTML = 'See Less <i class="fa-solid fa-angles-up" id="see-more-icon"></i>';
+  }
+}
+/*--js for show more projects ends--*/
